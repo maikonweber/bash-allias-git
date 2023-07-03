9dfbce0a49acaf424d64afd356f1557bf692cbe0
Maikon <m.carvalho@grouplinknetwork.com>
2023-07-02 23:22:57 -0300

    Add .

---
+++ gitbashalias.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+alias g='git'
+alias gs='git status'
+alias ga='git add'
+alias gc='git commit'
+alias gp='git push'
+alias gl='git log'
+alias gd='git diff'
+alias gb='git branch'
+alias gm='git merge'
+alias gr='git remote'
+alias gu='git pull'
